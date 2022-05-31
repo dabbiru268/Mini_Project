b5f4cb9ac5db3c127e3eff601dd8ab312cd84cd1
dabbiru268 <82017363+dabbiru268@users.noreply.github.com>
2022-05-31 19:12:28 +0530

    Add files via upload

---
+++ home.php
@@ -0,0 +1,32 @@
+<?php 
+session_start();
+
+if (isset($_SESSION['id']) && isset($_SESSION['user_name'])) {
+
+ ?>
+<!DOCTYPE html>
+<html>
+<head>
+	<title>HOME</title>
+	<link rel="stylesheet" type="text/css" href="style1.css">
+</head>
+<body>
+     <h1>Hello, <?php echo $_SESSION['name']; ?></h1>
+     <div>
+          <h4>Check your expenses here</h4>
+     </div>
+     <div class="links">
+            <ul>
+                <li><a href="expense_tracker.html">Expense Tracker</a></li>
+                <span><li><a href="logout.php">Logout</a></li></span>
+            </ul>
+     </div>
+</body>
+</html>
+
+<?php 
+}else{
+     header("Location: index.php");
+     exit();
+}
+ ?>
